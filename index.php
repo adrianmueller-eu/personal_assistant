472316f2c7ab668e576b54d021e721b60b3b21ed
Adrian Müller <dev@adrianmueller.eu>
2024-06-05 12:51:02 +0300

    Allow access for admins by default

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ $telegram = new Telegram($telegram_token, $chat_id, $DEBUG);
 $user_config_manager = new UserConfigManager($chat_id, $username, $name, $lang);
 $user_openai_api_key = $user_config_manager->get_openai_api_key();
 if ($user_openai_api_key == null || $user_openai_api_key == "") {
-    if ($global_config_manager->is_allowed_user($username, "general")) {
+    if ($is_admin || $global_config_manager->is_allowed_user($username, "general")) {
         $user_config_manager->set_openai_api_key($openai_api_key);
         $openai = new OpenAI($openai_api_key, $DEBUG);
     } else {
