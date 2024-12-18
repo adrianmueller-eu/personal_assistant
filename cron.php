a499852d70d19d46b50fbd9699afd92e2f0ca71f
Adrian Müller <dev@adrianmueller.eu>
2024-12-18 16:11:33 +0200

    cron currently broken (and unused)

--- cron.php
+++ cron.php
@@ -10,13 +10,15 @@ if ($DEBUG) {
 
 // imports
 require_once __DIR__."/lib/telegram.php";
-require_once __DIR__."/lib/openai.php";
+require_once __DIR__."/lib/LLM_connector.php";
 require_once __DIR__."/lib/logger.php";
 require_once __DIR__."/lib/global_config_manager.php";
 require_once __DIR__."/lib/user_config_manager.php";
 
 if ($DEBUG) {
     Log::set_echo_level(3);
+} else {
+    Log::set_echo_level(1);
 }
 
 // Tokens and keys
@@ -36,12 +38,6 @@ if ($chat_id_admin == null || $chat_id_admin == "") {
     Log::error("TELEGRAM_ADMIN_CHAT_ID is not set.");
     exit;
 }
-$openai_api_key = $global_config_manager->get("OPENAI_API_KEY");
-if ($openai_api_key == null || $openai_api_key == "") {
-    Log::error("OPENAI_API_KEY is not set.");
-    exit;
-}
-$openai = new OpenAI($openai_api_key, $DEBUG);
 
 // Get jobs from GlobalConfigManager
 $jobs = $global_config_manager->get_jobs();
@@ -88,7 +84,7 @@ for ($i = 0; $i < count($jobs); $i++) {
     // Check if the job is due
     if ($job->next_run != null && strtotime($job->next_run) > time()) {
         if ($DEBUG) {
-            Log::debug("Job \"".$job->name."\" is not due yet. Next run is not before ".$job->next_run.".");
+            Log::debug("Job \"$job->name\" is not due yet. Next run is not before $job->next_run.");
         }
         continue;
     }
@@ -101,68 +97,48 @@ for ($i = 0; $i < count($jobs); $i++) {
     }
 
     if ($job->is_prompt == "true") {  // "false" == true would validate to true!
-        // If chats/ folder has a file with name "$job->chat_id.json", load the user config
-        if (file_exists(__DIR__."/chats/".$job->chat_id.".json")) {
-            // Load the user config
-            $user_config_manager = new UserConfigManager($job->chat_id);
-            $config = clone $user_config_manager->get_config();
-            // Create a new OpenAI object with the user's API key
-            $openai_api_key = $user_config_manager->get_openai_api_key();
-            if (!$user_openai_api_key) {
-                $user_config_manager->set_openai_api_key($openai_api_key);
-            } else {
-                $openai_api_key = $user_openai_api_key;
-            }
-            $user_openai = new OpenAI($openai_api_key, $DEBUG);
-            // Set user's timezone
-            date_default_timezone_set($user_config_manager->get_timezone());
-            // If the last two messages are from the assistant, remove the last
-            $cnt = count($config->messages);
-            if ($cnt > 1 && $config->messages[$cnt - 1]->role == "assistant" && $config->messages[$cnt - 2]->role == "assistant") {
-                $user_config_manager->delete_messages(1);
-                array_pop($config->messages);
-            }
-            // Temporarily set the temperature to the job's temperature if it is set
-            if (isset($job->temperature) && $job->temperature != null) {
-                $config->temperature = $job->temperature;
-            }
-            // Replace variables in the message
-            $message = $job->message;
-            $message = str_replace("{{name}}", $user_config_manager->get_name(), $message);
-            $message = str_replace("{{time}}", date("g:ia"), $message);
-            $message = str_replace("{{date}}", date("l, F jS"), $message);
-            // Use a temporary system prompt to generate the next message
-            if ($user_config_manager->get_lang() == "de"){
-                $pre_message = "In der nächsten Nachricht, bitte antworte folgend dieser Anweisung:";
-            } else {
-                $pre_message = "In the next message, please include a response following this instruction:";
-            }
-            $config->messages[] = array(
-                "role" => "system",
-                "content" => $pre_message."\n\n".$message,
-            );
-            // Request a response from the model
-            $message = $user_openai->gpt($config, $user_config_manager);
-            // Add the message as assistant message
-            if (substr($message, 0, 7) != "Error: ") {
-                $user_config_manager->add_message("assistant", $message);
-            }
+        // Error if chats/ folder doesn't have a file with name "$job->chat_id.json"
+        if (!file_exists(__DIR__."/chats/$job->chat_id.json")) {
+            Log::error("User config for chat ID $job->chat_id not found.");
+            continue;
+        }
+        // Load the user config
+        $user_config_manager = new UserConfigManager($job->chat_id);
+        $config = clone $user_config_manager->get_config();
+        $llm = new LLMConnector($user_config_manager, $DEBUG);
+        date_default_timezone_set($user_config_manager->get_timezone());
+        // If the last two messages are from the assistant, remove the last
+        $cnt = count($config->messages);
+        if ($cnt > 1 && $config->messages[$cnt - 1]->role == "assistant" && $config->messages[$cnt - 2]->role == "assistant") {
+            $user_config_manager->delete_messages(1);
+            array_pop($config->messages);
+        }
+        // Temporarily set the temperature to the job's temperature if it is set
+        if (isset($job->temperature) && $job->temperature != null) {
+            $config->temperature = $job->temperature;
+        }
+        // Replace variables in the message
+        $message = $job->message;
+        $message = str_replace("{{name}}", $user_config_manager->get_name(), $message);
+        $message = str_replace("{{time}}", date("g:ia"), $message);
+        $message = str_replace("{{date}}", date("l, F jS"), $message);
+        // Use a temporary system prompt to generate the next message
+        if ($user_config_manager->get_lang() == "de"){
+            $pre_message = "In der nächsten Nachricht, bitte antworte folgend dieser Anweisung:";
         } else {
-            // Set default timezone
-            date_default_timezone_set($default_timezone);
-            // Copy the template config
-            $config = clone $default_config;  // clone, because we might execute multiple jobs in a row
-            // Update the temperature if it is set in the job
-            if (isset($job->temperature) && $job->temperature != null) {
-                $config["temperature"] = $job->temperature;
-            }
-            // Set the system prompt
-            $config["messages"] = array(array(
-                "role" => "system",
-                "content" => $job->message,
-            ));
-            // Request a response from the model using default openai object
-            $message = $openai->gpt($config, $user_config_manager);
+            $pre_message = "In the next message, please include a response following this instruction:";
+        }
+        $config->messages[] = array(
+            "role" => "system",
+            "content" => "$pre_message\n\n$message",
+        );
+        // Request a response from the model
+        $message = $llm->message($config);
+        // Add the message as assistant message
+        if (substr($message, 0, 7) != "Error: ") {
+            $user_config_manager->add_message("assistant", $message);
+        } else {
+            Log::error("Error in job \"$job->name\": ".$message);
         }
     } else {
         $message = $job->message;
